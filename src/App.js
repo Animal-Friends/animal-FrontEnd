6ea56db169d48e2ed576ee1decd6554cbeb1d890
jhva <junghun8158@naver.com>
2023-05-22 16:57:10 +0900

    :tada: build: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from "styled-component";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const GlobalStyle = createGlobalStyle`
+      body {
+        font-family: 'Noto Sans', sans-serif;
+        padding: 0;
+        margin: 0;
+        background: #F4F6F8;
+      }
+
+      div, p, span, h1 {
+        margin: 0;
+        box-sizing: border-box;
+        color: #474749;
+
+        ::-webkit-scrollbar {
+          display: none;
+        }
+      }
+
+      button, input {
+        outline: none;
+        border: none;
+      }
+
+      button {
+        cursor: pointer;
+      }
+    `;
+
+  return "1";
 }
 
 export default App;
