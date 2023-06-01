aa90a48129e05c54c009c7b0eb7d02f5e6fca373
jhva <junghun8158@naver.com>
2023-06-01 13:13:04 +0900

    :sparkles: feat: proxy 추가

--- src/api/url.js
+++ src/api/url.js
@@ -1,8 +1,8 @@
 // let url = 'http://127.0.0.1:8000/v1';
-let url = "http://43.200.253.205:3100/";
+let url = "https://www.animalfriends-server.site/";
 
 if (process.env.NODE_ENV === "production") {
-  url = "http://43.200.253.205:3100/";
+  url = "https://www.animalfriends-server.site/";
 }
 
 export const BASE_URL = url;
